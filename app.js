b2601e402c2acf4ffd857941872369bd25924876
harpalsingh1234 <harpalsingh082116@gmail.com>
2025-02-20 19:47:14 +0530

    7

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-document.querySelector('.cross').Style.display='none';
+document.querySelector('.cross').style.display='none';
 document.querySelector('.hamburger').addEventListener('click',()=>{
     document.querySelector('.sidebar').classList.toggle('sidebarGo');
     if(document.querySelector('.sidebar').classList.contains('sidebarGo')){
