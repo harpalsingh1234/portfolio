c5dd828e3df143bd622829ae329c8713b8762721
harpalsingh1234 <harpalsingh082116@gmail.com>
2025-02-20 19:24:16 +0530

    4

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
-console.log{"script running..."}
+document.querySelector('.cross').Style.display='none';
+document.querySelector('.cross').Style.display='none';
+document.querySelector('.hamburger').addEventListener('click',()=>{
+    document.querySelector('.sidebar').classList.toggle('sidebarGo');
+    if(document.querySelector('.sidebar').classList.contains('sidebarGo')){
+        document.querySelector('.hamb').style.display='inline'
+        document.querySelector('.cross').style.display='none'
+    }
+    else{
+        document.querySelector('.hamb').style.display='none'
+        setTimeout(()=>{
+        document.querySelector('.cross').style.display='inline'
+     },300);
+   }
+})
 
