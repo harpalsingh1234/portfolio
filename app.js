7cc6879b9f8be8b9c43207ebbac3fe795a767941
harpalsingh1234 <harpalsingh082116@gmail.com>
2025-02-20 20:10:47 +0530

    0.1

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+console.log('script running..')
 document.querySelector('.cross').style.display='none';
 document.querySelector('.hamburger').addEventListener('click',()=>{
     document.querySelector('.sidebar').classList.toggle('sidebarGo');
