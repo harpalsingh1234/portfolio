db2f7727d3a641551f3450df69d1aa1115332e8d
harpalsingh1234 <harpalsingh082116@gmail.com>
2025-02-20 20:41:01 +0530

    0.4

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-console.log('script running..')
+console.log('script running...')
 document.querySelector('.cross').style.display='none';
 document.querySelector('.hamburger').addEventListener('click',()=>{
-    document.querySelector('.sidebar').classList.toggle('sidebarGo');
-    if(document.querySelector('.sidebar').classList.contains('sidebarGo')){
-        document.querySelector('.hamb').style.display='inline'
-        document.querySelector('.cross').style.display='none'
+    document.querySelector('.sidebar').classList.toggle('.sidebarGo');
+     if(document.querySelector('.sidebar').classList.contains('sidebarGo')){
+         document.querySelector('.hamb').style.display='inline'
+         document.querySelector('.cross').style.display='none'
     }
     else{
         document.querySelector('.hamb').style.display='none'
         setTimeout(()=>{
         document.querySelector('.cross').style.display='inline'
-     },300);
+  },300);
    }
-})
+ });
 
