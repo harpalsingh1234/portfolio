dcde9107afd34c21cd62323d5d205d32e262225f
harpalsingh1234 <harpalsingh082116@gmail.com>
2025-02-21 14:03:32 +0530

    0.55

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
-console.log('script running...')
+console.log("scsript running...")
 document.querySelector('.cross').style.display='none';
-document.querySelector('.hamburger').addEventListener('click',()=>{
-    document.querySelector('.sidebar').classList.toggle('.sidebarGo');
-     if(document.querySelector('.sidebar').classList.contains('sidebarGo')){
-         document.querySelector('.hamb').style.display='inline'
-         document.querySelector('.cross').style.display='none'
-    }
-    else{
+document.querySelector('.hamburger').addEventListener("click", ()=>{
+    document.querySelector(".sidebar").classList.toggle('sidebarGo');
+    if(document.querySelector('.sidebar').classList.contains('sidebarGo')){
+        document.querySelector('.hamb').style.display='inline'
+        document.querySelector('.cross').style.display='none'
+    }else{
         document.querySelector('.hamb').style.display='none'
-        setTimeout(()=>{
         document.querySelector('.cross').style.display='inline'
-  },300);
-   }
- });
-
+    }
+});
